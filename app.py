895b9fed1d0edd3e4bf83dc366526d6b2a46b140
Hamid Reza <hamid@example.com>
2025-08-25 23:44:41 +0330

    fix: apply real dynamic port for railway

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def home():
     return "✅ Hello from Flask on Railway!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # این خط خیلی خیلی حیاتیه!
-    print(f"⚡ Starting Flask app on port: {port} ...")  # لاگ ساده برای تست تو لاگ‌ها
+    port = int(os.environ.get('PORT', 5000))
+    print(f"⚡ Starting Flask app on port: {port} ...")
     app.run(host='0.0.0.0', port=port)
